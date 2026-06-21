34d66519635734759c9de5e1fb3aa65e9041b7e2
24072ap <24072ap@hvhs.school.nz>
2026-06-21 01:33:27 +0000

    Jun 21, 2026, 1:33 AM

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Hello world!")
+ 
+ 
+ console.log("Running t05_JavaScript_and_HTML.js");
+ 
+function start(){
+
+
+  
+  let shoppingChoice = SHOPPING_FIELD.value;
+  shoppingArray.push(shoppingChoice);
+  OUTPUT.innerHTML += "You have added: "+shoppingChoice+ "  to the list "+"<br> <br> <br><br>";
+
+}
+
+function ButtonClick(){
+  OUTPUT.innerHTML += "These are the items on your shopping list:<br>";
+  for(let i=0; i<shoppingArray.length; i++){
+    OUTPUT.innerHTML += shoppingArray[i] + "<br>";
+  }
+ 
+ const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+ OUTPUT.innerHTML = "<h2>Added by Javascript</h2>";
+  
