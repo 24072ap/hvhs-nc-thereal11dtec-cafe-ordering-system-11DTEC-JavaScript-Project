92cfa424c5a993c1d4d02710dc97771bda14ab02
24072ap <24072ap@hvhs.school.nz>
2026-06-27 21:44:39 +0000

    Jun 27, 2026, 9:44 PM

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const menufood = [ "Chocolate Muffin $4", "Chocolate Pastry $7"] //array2
 const itemPrice2 = [4, 7];
 
 
-  if (AGE_FIELD.value < 13 || AGE_FIELD.value > 99){
+  if (CASH_FIELD.value < 13 || CASH_FIELD.value > 99){
         OUTPUT.innerHTML = "You're not a good age<br>";
     }
 
