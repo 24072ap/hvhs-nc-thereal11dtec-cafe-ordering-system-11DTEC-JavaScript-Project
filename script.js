b016d85f6d7b65a817219c14b60d53e7aed8b0fd
24072ap <24072ap@hvhs.school.nz>
2026-06-27 21:25:48 +0000

    Jun 27, 2026, 9:25 PM

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const menudrink = ["Latte $5", "Cappucino $5", "Mocha $5"] //array
 const itemPrice = [5, 5, 5];
 const menufood = [ "Chocolate Muffin $4", "Chocolate Pastry $7"] //array2
 const itemPrice2 = [4, 7];
-  
+  z
   let shoppingChoice = SHOPPING_FIELD.value;
   shoppingArray.push(shoppingChoice);
   OUTPUT.innerHTML += "You have added: "+shoppingChoice+ "  to the list "+"<br> <br> <br><br>";
